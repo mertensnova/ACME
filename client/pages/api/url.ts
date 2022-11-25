f4f3d8500863dd2210ad0b846e78a519be5005ad
amr8644 <amr8644@gmail.com>
2022-11-25 17:01:17 +0300

    Fixed issues #3

--- client/pages/api/url.ts
+++ client/pages/api/url.ts
@@ -1,4 +1,1 @@
-export const API_URL =
-  process.env.ENV !== "production"
-    ? "http://localhost:8080"
-    : "https://go-services.onrender.com";
+export const API_URL = "https://go-services.onrender.com";
