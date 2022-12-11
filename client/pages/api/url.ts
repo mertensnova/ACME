071bb32d526c1cd4a202d14970155aee8e104f5b
amr8644 <amr8644@gmail.com>
2022-12-11 18:38:49 +0300

    Added enviroment variable

--- client/pages/api/url.ts
+++ client/pages/api/url.ts
@@ -1,1 +1,4 @@
-export const API_URL = "https://go-services.onrender.com";
+export const API_URL =
+   process.env.NODE_ENV == "production"
+      ? "https://go-services.onrender.com"
+      : "http://localhost:8080";
